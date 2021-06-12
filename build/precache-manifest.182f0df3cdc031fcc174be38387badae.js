571c69209a88af21985700185c6cc56f741a3bc8
unknown <ishworineupane135@gmail.com>
2021-06-12 16:54:57 +0545

    add: initialCommit

---
+++ build/precache-manifest.182f0df3cdc031fcc174be38387badae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aabc1e42746d29a407b286b12345fdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d487856d8dc45357d4e7",
+    "url": "/static/css/main.72cd2747.chunk.css"
+  },
+  {
+    "revision": "241f86801c278ad63a97",
+    "url": "/static/js/2.67f5ef00.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.67f5ef00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d487856d8dc45357d4e7",
+    "url": "/static/js/main.8aa7762b.chunk.js"
+  },
+  {
+    "revision": "59bde5ba28a8a5d646c1",
+    "url": "/static/js/runtime-main.f626648b.js"
+  }
+]);
